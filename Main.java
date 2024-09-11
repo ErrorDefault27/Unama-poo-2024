3a0522ca1b7ce3dc9b98b20fa9e301f8246d2dd1
ErrorDefault27 <edgarcau27@gmail.com>
2024-09-11 12:39:45 +0000

    add: aula 4

--- Main.java
+++ Main.java
@@ -1,30 +1,81 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 class Tarefa {
-        private String titulo;
-        private String descricao;
-        private String prazo;
-        private int prioridade = 10;
-
-        public Tarefa(String titulo, String descricao, String prazo, int prioridade){
-            this.titulo = titulo;
-            this.descricao = descricao;
-            this.prazo = prazo;
-            this.prioridade = prioridade;
-        }
-        public String getTitulo() {
-
-            return this.titulo;
-
-        }
-        public void setTitulo(String titulo){
-
-            this.titulo = titulo;
-
-        }
+        
+    private String titulo;
+    private String descricao;
+    private String prazo;
+    private int prioridade = 10;
+//construtor 1: completo
+    public Tarefa(String titulo, String descricao, String prazo, int prioridade){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prazo = prazo;
+        this.prioridade = prioridade;
     }
+    // construtor com menos paramentros
+    public Tarefa(String titulo, String prazo) {
+        this.titulo = titulo;
+        this.prazo = prazo;
+        this.descricao = "";
+        this.prioridade = 0;
+    }
+    
+    public String getTitulo() {
+
+        return this.titulo;
+
+    }
+    public String getDescricao() {
+
+        return this.descricao;
+
+    }
+    public String getPrazo() {
+
+        return this.prazo;
+
+    }
+    public int getPrioridade() {
+
+        return this.prioridade;
+
+    }
+    public void setTitulo(String titulo){
+
+        this.titulo = titulo;
+
+    }
+    public void exibirDetalhes() {
+        System.out.println("Título: " + this.titulo);
+        System.out.println("Descrição: " + this.descricao);
+        System.out.println("Prazo: " + this.prazo);
+        System.out.println("Prioridade: " + this.prioridade);
+    }
+    public long calcularDiasRestantes() {
+        LocalDate dataPrazo = LocalDate.parse(this.prazo);
+        LocalDate hoje = LocalDate.now();
+
+        return ChronoUnit.DAYS.between(hoje,dataPrazo);
+    }
+}
 
 public class Main {
-    public static void main(String[] args) {
-        Tarefa tarefa1 = new Tarefa("Estudar poo", "a", "a", 1);
-        System.out.println(tarefa1.getTitulo());
-    }
+public static void main(String[] args) {
+    Tarefa tarefa1 = new Tarefa("Estudar POO", "Revisar os conceitos de classes e objetos", "2024-09-15", 1);
+    System.out.println("========Imprimindo com get==========");
+    System.out.println(tarefa1.getTitulo());
+    System.out.println(tarefa1.getDescricao());
+    System.out.println(tarefa1.getPrazo());
+    System.out.println(tarefa1.getPrioridade());
+    System.out.println("========Imprimindo com o construtor 1==========");
+    tarefa1.exibirDetalhes();
+    
+    System.out.println("========Imprimindo com o construtor 2==========");
+    Tarefa tarefa2 = new Tarefa("Estudar para provas", "2050-10-25");
+    tarefa2.exibirDetalhes();
+
+    System.out.println("Dias restantes para entregar a tarefa 1: " + tarefa1.calcularDiasRestantes());
+}
 }
